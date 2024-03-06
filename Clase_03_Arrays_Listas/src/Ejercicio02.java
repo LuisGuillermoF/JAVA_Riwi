c53d0ed0add2cb97c606b56c4dd1d8709c055ffc
JhonatanTH7 <jhonatantorohurtado93@gmail.com>
2024-03-06 11:49:23 -0500

    funciones clase 3

--- Clase_03_Arrays_Listas/src/Ejercicio02.java
+++ Clase_03_Arrays_Listas/src/Ejercicio02.java
@@ -22,32 +22,17 @@ public class Ejercicio02 {
 
             switch (Menu) {
                 case 1:
-                    String Fila = JOptionPane.showInputDialog(null, "Ingrese la fila (1-5)");
-                    int numberFila = Integer.parseInt(Fila);
-                    String Column = JOptionPane.showInputDialog(null, "Ingrese el asiento (1-10)");
-                    int numberColumn = Integer.parseInt(Column);
-
-                    if (asientos[numberFila][numberColumn] == false) {
-                        asientos[numberFila][numberColumn] = true;
-                        JOptionPane.showMessageDialog(null, "Asiento reservado correctamente");
-                    }
+                   addPerson(asientos);
+                   break;
                 case 2:
-                    String FilaDelete = JOptionPane.showInputDialog(null, "Ingrese la fila (1-5)");
-                    int numberFilaDelete = Integer.parseInt(FilaDelete);
-                    String ColumnDelete = JOptionPane.showInputDialog(null, "Ingrese el asiento (1-10)");
-                    int numberColumnDelete = Integer.parseInt(ColumnDelete);
-
-                    if (asientos[numberFilaDelete][numberColumnDelete] == false) {
-                        JOptionPane.showMessageDialog(null, "Asiento no ocupado");
-                    } else {
-                        asientos[numberFilaDelete][numberColumnDelete] = false;
-                        JOptionPane.showMessageDialog(null, "Asiento borrado");
-                    }
+                    deletePerson(asientos);
+                    break;
                 case 3:
                     printFilas(asientos);
                     break;
-
-
+                case 4:
+                    countTotal(asientos);
+                    break;
             }
 
         } while (Menu != 5);
@@ -56,15 +41,59 @@ public class Ejercicio02 {
     }
 
 
-public static void printFilas(boolean[][] asientos) {
-    JOptionPane.showMessageDialog(null, "Asientos disponibles");
-    String cine = "";
-    for (int i = 0; i <= 5; i++) {
-        for (int j = 0; j <= 10; j++) {
-            cine += (i + 1) + " - " + (j + 1)+ " P ";
+    public static void deletePerson(boolean[][] delete){
+        String FilaDelete = JOptionPane.showInputDialog(null, "Ingrese la fila (1-5)");
+        int numberFilaDelete = Integer.parseInt(FilaDelete);
+        String ColumnDelete = JOptionPane.showInputDialog(null, "Ingrese el asiento (1-10)");
+        int numberColumnDelete = Integer.parseInt(ColumnDelete);
+
+        if (delete[numberFilaDelete][numberColumnDelete] == true) {
+            JOptionPane.showMessageDialog(null, "Asiento no ocupado");
+        } else {
+            delete[numberFilaDelete][numberColumnDelete] = false;
+            JOptionPane.showMessageDialog(null, "Asiento borrado");
         }
-        cine+="\n";
     }
-    JOptionPane.showMessageDialog(null, "Disponibles" +"\n"+ cine);
-}
+
+    public static void addPerson(boolean[][] agg){
+        String Fila = JOptionPane.showInputDialog(null, "Ingrese la fila (1-5)");
+        int numberFila = Integer.parseInt(Fila);
+        String Column = JOptionPane.showInputDialog(null, "Ingrese el asiento (1-10)");
+        int numberColumn = Integer.parseInt(Column);
+
+        if (!agg[numberFila][numberColumn]) {
+            agg[numberFila][numberColumn] = true;
+            JOptionPane.showMessageDialog(null, "Asiento reservado correctamente");
+        }
+    }
+
+    public static void printFilas(boolean[][] asientos) {
+        JOptionPane.showMessageDialog(null, "Asientos disponibles");
+        String cine = "";
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 10; j++) {
+
+                cine += (i + 1) + " - " + (j + 1) + " P ";
+            }
+            cine += "\n";
+        }
+        JOptionPane.showMessageDialog(null, "Disponibles" + "\n" + cine);
+    }
+
+    public static void countTotal(boolean [][] count){
+        String ocupados = "";
+        String libres = "";
+
+        for (int i = 0;i<=5;i++){
+            for(int j=0;j<=10;j++){
+                if (count[i][j] == true){
+                    ocupados += j;
+                }else {
+                    libres += j;
+                }
+            }
+        }
+        JOptionPane.showMessageDialog(null,"Los asinetos libres son: "+libres+ " Los asientos ocupados son:"+ ocupados);
+    }
+
 }
