40cae2eba2524a22a2e396c2b9d9eae172fe2cd7
Luis Guiller Foronda <morganixxD@gmail.com>
2024-03-18 13:17:39 -0500

    Todo

--- Gestion_Cursos_06/src/Cursos/Curso.java
+++ Gestion_Cursos_06/src/Cursos/Curso.java
@@ -1,2 +1,55 @@
-package Cursos;public class Curso {
+package Cursos;
+
+import Estudiantes.Estudiante;
+
+import java.util.ArrayList;
+
+public class Curso {
+
+    private String codigo;
+
+    private String nombre;
+
+    private ArrayList<Estudiante> listaEstudiantes;
+
+
+    public Curso(String codigo, String nombre){
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.listaEstudiantes = new ArrayList<>();
+    }
+
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Estudiante> getListaEstudiantes() {
+        return listaEstudiantes;
+    }
+
+    public void setListaEstudiantes(ArrayList<Estudiante> listaEstudiantes) {
+        this.listaEstudiantes = listaEstudiantes;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Curso " +
+                "codigo= '" + codigo + '\'' +
+                ", nombre= '" + nombre + '\'' +
+                ", listaEstudiantes= " + listaEstudiantes ;
+    }
 }
